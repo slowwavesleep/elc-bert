01117e3b165c4e31c3f42bb7848790b7dc7e6868
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2024-05-31 21:04:16 +0300

    Add roberta style special tokens to dataset

--- pre_training/dataset.py
+++ pre_training/dataset.py
@@ -1,18 +1,21 @@
 import torch
 from torch.utils.data import Dataset
 from smart_open import open
+from tokenizers import processors
 
 # NOTE Change to better masking?
 
 
 class SpanMaskingStrategy:
-    def __init__(self,
-                 mask_p,
-                 tokenizer,
-                 n_special_tokens,
-                 padding_label_id=-100,
-                 random_p=0.1,
-                 keep_p=0.1):
+    def __init__(
+        self,
+        mask_p,
+        tokenizer,
+        n_special_tokens,
+        padding_label_id=-100,
+        random_p=0.1,
+        keep_p=0.1,
+    ):
         self.mask_p = mask_p
         self.random_p = random_p
         self.keep_p = keep_p
@@ -27,22 +30,21 @@ class SpanMaskingStrategy:
 
         n_masked = torch.binomial(
             (tokens >= self.n_special_tokens).float().sum(dim=0, keepdim=True),
-            torch.FloatTensor([self.mask_p])
+            torch.FloatTensor([self.mask_p]),
         ).item()
         preservation_mask = tokens < self.n_special_tokens
         mask = torch.zeros_like(tokens, dtype=torch.bool)
         counter = 100
 
         while n_masked > mask.long().sum() and counter > 0:
-            span_length = torch.tensor([0]).geometric_(1/3).item() % 10
+            span_length = torch.tensor([0]).geometric_(1 / 3).item() % 10
             offset = torch.randint(
-                -(span_length - 1),
-                tokens.size(0) + span_length,
-                []
+                -(span_length - 1), tokens.size(0) + span_length, []
             ).item()
             sub_mask = torch.zeros_like(tokens, dtype=torch.bool)
-            sub_mask[max(0, offset):min(mask.size(0)-1,
-                     offset + span_length)] = True
+            sub_mask[max(0, offset) : min(mask.size(0) - 1, offset + span_length)] = (
+                True
+            )
             sub_mask[preservation_mask] = False
 
             random_p = torch.rand([]).item()
@@ -56,7 +58,7 @@ class SpanMaskingStrategy:
                     low=self.n_special_tokens - 1,
                     high=self.tokenizer.get_vocab_size(),
                     size=(sub_mask.sum(),),
-                    dtype=torch.long
+                    dtype=torch.long,
                 )
                 inputs[sub_mask] = random_words
             else:
@@ -71,16 +73,18 @@ class SpanMaskingStrategy:
 
 
 class Dataset(Dataset):
-    def __init__(self,
-                 file: str,
-                 offset: int,
-                 n_gpus: int,
-                 tokenizer,
-                 seq_length: int = 512,
-                 mask_p: float = 0.15,
-                 short_p: float = 0.1,
-                 random_p: float = 0.1,
-                 keep_p: float = 0.1):
+    def __init__(
+        self,
+        file: str,
+        offset: int,
+        n_gpus: int,
+        tokenizer,
+        seq_length: int = 512,
+        mask_p: float = 0.15,
+        short_p: float = 0.1,
+        random_p: float = 0.1,
+        keep_p: float = 0.1,
+    ):
         self.tokenizer = tokenizer
 
         self.seq_length = seq_length
@@ -93,13 +97,21 @@ class Dataset(Dataset):
             self.n_special_tokens,
             padding_label_id=-100,
             random_p=random_p,
-            keep_p=keep_p
+            keep_p=keep_p,
         )
-
-        self.mask_index = self.tokenizer.token_to_id("[MASK]")
-        self.cls_index = self.tokenizer.token_to_id("[CLS]")
-        self.sep_index = self.tokenizer.token_to_id("[SEP]")
-        self.pad_index = self.tokenizer.token_to_id("[PAD]")
+        # TODO generalize
+        if self.tokenizer.post_processor is not None and isinstance(
+            self.tokenizer.post_processor, processors.RobertaPostProcessor
+        ):
+            self.mask_index = self.tokenizer.token_to_id("<mask>")
+            self.cls_index = self.tokenizer.token_to_id("<s>")
+            self.sep_index = self.tokenizer.token_to_id("</s>")
+            self.pad_index = self.tokenizer.token_to_id("<pad>")
+        else:
+            self.mask_index = self.tokenizer.token_to_id("[MASK]")
+            self.cls_index = self.tokenizer.token_to_id("[CLS]")
+            self.sep_index = self.tokenizer.token_to_id("[SEP]")
+            self.pad_index = self.tokenizer.token_to_id("[PAD]")
 
         self.segments = []
         for i, segment in enumerate(open(file)):
@@ -123,19 +135,27 @@ class Dataset(Dataset):
     def __getitem__(self, index):
         tokens = self.segments[index]
 
-        target_seq_length = self.seq_length - 2 \
-            if self.rand() > self.short_p \
+        target_seq_length = (
+            self.seq_length - 2
+            if self.rand() > self.short_p
             else self.randint(1, self.seq_length - 2)
+        )
         tokens = tokens[:target_seq_length]
         padding_length = (self.seq_length - 2) - len(tokens)
-        segment = [self.cls_index] + tokens + [self.sep_index] \
+        segment = (
+            [self.cls_index]
+            + tokens
+            + [self.sep_index]
             + [self.pad_index] * padding_length
+        )
         segment = torch.LongTensor(segment)
 
-        attention_mask = torch.cat([
-            torch.zeros(len(tokens) + 2, dtype=torch.bool),
-            torch.ones(padding_length, dtype=torch.bool)
-        ])
+        attention_mask = torch.cat(
+            [
+                torch.zeros(len(tokens) + 2, dtype=torch.bool),
+                torch.ones(padding_length, dtype=torch.bool),
+            ]
+        )
 
         inputs, outputs = self.masking_strategy(segment)
 
@@ -143,8 +163,10 @@ class Dataset(Dataset):
 
     def show_random_item(self):
         inputs, _, outputs = self.__getitem__(self.randint(0, len(self)))
-        print(' '.join(self.tokenizer.id_to_token(i) for i in inputs),
-              flush=True)
-        print(' '.join(self.tokenizer.id_to_token(o)
-                       if o >= 0 else "-1" for o in outputs),
-              flush=True)
+        print(" ".join(self.tokenizer.id_to_token(i) for i in inputs), flush=True)
+        print(
+            " ".join(
+                self.tokenizer.id_to_token(o) if o >= 0 else "-1" for o in outputs
+            ),
+            flush=True,
+        )
