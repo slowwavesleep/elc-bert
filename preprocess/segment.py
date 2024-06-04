feb344c443c2422a375f71ec05d760efa8cc81c6
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2024-06-04 15:22:44 +0300

    Add progress bar to segment.py

--- preprocess/segment.py
+++ preprocess/segment.py
@@ -1,14 +1,20 @@
+from tqdm import tqdm
 import nltk
 
+in_path = "../data/processed/all.txt"
+out_path = "../data/processed/segmented.txt"
 
-with open("../data/processed/segmented.txt", "w") as f:
-    for line in open("../data/processed/all.txt"):
+with open(in_path) as file:
+    total_lines = sum(1 for line in file)
+
+with open(out_path, "w") as out_file, open(in_path) as in_file:
+    for line in tqdm(in_file, total=total_lines):
         line = line.strip()
 
         if len(line) == 0:
-            f.write('\n')
+            out_file.write("\n")
             continue
 
         sentences = nltk.sent_tokenize(line)
-        sentences = '\n'.join(sentences) 
-        f.write(f"{sentences}[PAR]\n")
+        sentences = "\n".join(sentences)
+        out_file.write(f"{sentences}[PAR]\n")
